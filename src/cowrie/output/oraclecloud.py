8ef38ad06427056ba7a17f52b482d0d2dbf63553
ubuntu <ubuntu@ip-10-0-1-48>
2025-01-29 12:14:16 +0000

    waleko__gemini-pro-1.5_36da__id_195

--- src/cowrie/output/oraclecloud.py
+++ src/cowrie/output/oraclecloud.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 import json
-from configparser import NoOptionError
-
-import oci
+import datetime
 import secrets
 import string
-import oci
-from oci import auth
-import datetime
 
+import oci
 import cowrie.core.output
 from cowrie.core.config import CowrieConfig
 
@@ -21,7 +17,7 @@ class Output(cowrie.core.output.Output):
 
     def generate_random_log_id(self):
         charset = string.ascii_letters + string.digits
-        random_log_id = ''.join(secrets.choice(charset) for _ in range(32))
+        random_log_id = "".join(secrets.choice(charset) for _ in range(32))
         return f"cowrielog-{random_log_id}"
 
 
@@ -51,13 +47,12 @@ class Output(cowrie.core.output.Output):
                 timestamp_opc_agent_processing=current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
         except oci.exceptions.ServiceError as ex:
             print(
-                f"Oracle Cloud plugin Error: {ex.message}\n" +
-                f"Oracle Cloud plugin Status Code: {ex.status}\n"
+                f"Oracle Cloud plugin Error: {ex.message}\n"
+                f"Oracle Cloud plugin Status Code: {ex.status}\n" # Fixed print - t201
             )
         except Exception as ex:
-            print(f"Oracle Cloud plugin Error: {ex}")
-            raise
-            
+            print(f"Oracle Cloud plugin Error: {ex}") # Fixed print - t201
+            raise # re-raise after logging
 
     def start(self):
         """
