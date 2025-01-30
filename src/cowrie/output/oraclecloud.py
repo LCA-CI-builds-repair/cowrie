883a216ff166392394b05252006812109dbf8fa7
ubuntu <ubuntu@ip-10-0-1-49>
2025-01-30 12:01:39 +0000

    waleko__claude-3-opus_1637__id_195

--- src/cowrie/output/oraclecloud.py
+++ src/cowrie/output/oraclecloud.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 import json
-from configparser import NoOptionError
+from configparser import NoOptionError  # noqa: F401
 
 import oci
 import secrets
 import string
-import oci
 from oci import auth
 import datetime
 
@@ -21,7 +20,7 @@ class Output(cowrie.core.output.Output):
 
     def generate_random_log_id(self):
         charset = string.ascii_letters + string.digits
-        random_log_id = ''.join(secrets.choice(charset) for _ in range(32))
+        random_log_id = "".join(secrets.choice(charset) for _ in range(32))
         return f"cowrielog-{random_log_id}"
 
 
@@ -50,12 +49,12 @@ class Output(cowrie.core.output.Output):
                             type="cowrie")]),
                 timestamp_opc_agent_processing=current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
         except oci.exceptions.ServiceError as ex:
-            print(
+            self.log(
                 f"Oracle Cloud plugin Error: {ex.message}\n" +
                 f"Oracle Cloud plugin Status Code: {ex.status}\n"
             )
         except Exception as ex:
-            print(f"Oracle Cloud plugin Error: {ex}")
+            self.log(f"Oracle Cloud plugin Error: {ex}")
             raise
             
 
