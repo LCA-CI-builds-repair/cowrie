e6b328e1e9c7aeed7236cf841956167e78879100
ubuntu <ubuntu@ip-10-0-1-79>
2025-01-30 17:03:42 +0000

    waleko__claude-3-opus_c087__id_195

--- src/cowrie/output/oraclecloud.py
+++ src/cowrie/output/oraclecloud.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 import json
-from configparser import NoOptionError
+from configparser import ConfigParser, NoOptionError
 
 import oci
 import secrets
-import string
-import oci
 from oci import auth
 import datetime
 
@@ -21,7 +19,7 @@ class Output(cowrie.core.output.Output):
 
     def generate_random_log_id(self):
         charset = string.ascii_letters + string.digits
-        random_log_id = ''.join(secrets.choice(charset) for _ in range(32))
+        random_log_id = "".join(secrets.choice(charset) for _ in range(32))
         return f"cowrielog-{random_log_id}"
 
 
@@ -50,12 +48,12 @@ class Output(cowrie.core.output.Output):
                             type="cowrie")]),
                 timestamp_opc_agent_processing=current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
         except oci.exceptions.ServiceError as ex:
-            print(
+            self.error(
                 f"Oracle Cloud plugin Error: {ex.message}\n" +
                 f"Oracle Cloud plugin Status Code: {ex.status}\n"
             )
         except Exception as ex:
-            print(f"Oracle Cloud plugin Error: {ex}")
+            self.error(f"Oracle Cloud plugin Error: {ex}")
             raise
             
 
@@ -87,7 +85,7 @@ class Output(cowrie.core.output.Output):
                 "tenancy": tenancy_ocid,
                 "region": region
             }
-            oci.config.validate_config(config_with_key_content)
+            self.validate_config(config_with_key_content)
             self.loggingingestion_client = oci.loggingingestion.LoggingClient(config_with_key_content)
         else:
             raise ValueError("Invalid authentication type")
@@ -105,3 +103,8 @@ class Output(cowrie.core.output.Output):
             if i.startswith("log_"):
                 del logentry[i]
         self.sendLogs(json.dumps(logentry))
+
+    @staticmethod    
+    def validate_config(config):
+        config_parser = ConfigParser()
+        config_parser.read_dict(config)
